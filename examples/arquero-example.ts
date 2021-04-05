c0fc863f31b07ef0af14baec86dd91436fc84f3a
Idan Gazit <idan@gazit.me>
2021-04-05 13:55:55 -0700

    Add arquero example

---
+++ examples/arquero-example.ts
@@ -0,0 +1,26 @@
+import * as aq from "https://cdn.skypack.dev/arquero";
+
+let t = await aq.fromCSV(await Deno.readTextFile("flights.csv"));
+
+
+t = t
+  .derive({
+    // convert the JSON duration representation into seconds
+    // input: {hours: 1, minutes: 2, seconds: 3}
+    // output: 3723
+    duration: (d: any) => {
+      const parsed = aq.op.parse_json(d.duration);
+      return (
+        (parsed.days ?? 0) * 24 * 60 * 60 +
+        (parsed.hours ?? 0) * 60 * 60 +
+        (parsed.minutes ?? 0) * 60 +
+        (parsed.seconds ?? 0)
+      )
+    },
+    // convert the date from Unix millis to ISO8601
+    day: (d: any) => aq.op.format_utcdate(d.day, true),
+  })
+  // Sort rows by duration, descreasing
+  .orderby(aq.desc("duration"));
+
+await Deno.writeTextFile("flights_cleaned.csv", t.toCSV());
