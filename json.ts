dbb39fb4b69d8072724170435c7b1052c2a392aa
Idan Gazit <idan@gazit.me>
2021-04-05 13:05:40 -0700

    Eliminate file.ts
    Deno.readTextFile/writeTextFile is sufficient

--- json.ts
+++ json.ts
@@ -1,9 +1,7 @@
-import {readFile, writeFile} from './file.ts'
-
 export async function readJSON(path: string, encoding = 'utf-8'): Promise<unknown> {
-  return JSON.parse(await readFile(path))
+  return JSON.parse(await Deno.readTextFile(path))
 }
 
 export async function writeJSON(path: string, data: any) {
-  await writeFile(path, JSON.stringify(data))
+  await Deno.writeTextFile(path, JSON.stringify(data))
 }
