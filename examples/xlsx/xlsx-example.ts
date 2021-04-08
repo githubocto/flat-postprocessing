7cbf6ff62daa1c9893903ec8af0ce4de36987911
Irene Alvarado <irealva@github.com>
2021-04-08 13:15:17 -0700

    adding xlsx functions. mainly to support type checking

---
+++ examples/xlsx/xlsx-example.ts
@@ -0,0 +1,14 @@
+import { xlsx, readXLSX } from '../../xlsx.ts'
+import { writeCSV } from '../../csv.ts'
+
+const inputFilename = './examples/xlsx/prices.xlsx'
+const outputFilename = './examples/xlsx/prices.csv'
+
+// read about what the xlsx library can do here: https://github.com/SheetJS/sheetjs
+
+const workbook = await readXLSX(inputFilename)
+const sheetData = workbook.Sheets[workbook.SheetNames[0]]
+const csvString = await xlsx.utils.sheet_to_csv(sheetData) // can use to_json, to_txt, to_html, to_formulae
+
+// Write to csv
+writeCSV(outputFilename, csvString)
--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from './json.ts'
 export * from './csv.ts'
 export * from './image.ts'
+export * from './xlsx.ts'
