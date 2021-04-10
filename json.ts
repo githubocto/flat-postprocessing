3ef9e8a2442ab14276df60f6ba8194688462a64a
Irene Alvarado <irealva@github.com>
2021-04-09 17:08:21 -0700

    Add a file for reading json from URL

--- json.ts
+++ json.ts
@@ -1,7 +1,13 @@
-export async function readJSON(path: string, encoding = 'utf-8'): Promise<unknown> {
+export async function readJSON(path: string) {
   return JSON.parse(await Deno.readTextFile(path))
 }
 
 export async function writeJSON(path: string, data: any) {
   await Deno.writeTextFile(path, JSON.stringify(data))
+}
+
+export async function readJSONFromURL(url: string) {
+  const response = await fetch(url)
+  const json = await response.json()
+  return json
 }
