f11008ad15784ddfe102f6a0595e04da3424971f
Irene Alvarado <irealva@github.com>
2022-03-07 09:41:18 -0800

    fix sheets example given Sheets API change

--- examples/sheets/sheets-example.ts
+++ examples/sheets/sheets-example.ts
@@ -2,26 +2,11 @@ import { Parser } from 'https://deno.land/x/xmlparser@v0.2.0/mod.ts'
 
 // Example of how to fetch and read a Google spreadsheet
 // For this to work without authentication, you have to publish the spreadsheet to the web
-// File > Publish to the web > publish entire document as a web page
-const sheetId = '1coaXmKes8b3GUFDGbmb-Hj4bek1HOEI-WCmOF4tIHwI'
-const sheetNumber = 1;
-const url = `https://spreadsheets.google.com/feeds/cells/${sheetId}/${sheetNumber}/public/values` // defaults to XML format
-// const url = `https://spreadsheets.google.com/feeds/cells/${sheetId}/${sheetNumber}/public/values?alt=json` // alternative to fetch as json
+// File > Publish to the web > publish entire document as a csv file
+
+const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQT7KW2U7o4zh2Sh5qpTl42OQzbKZG-glDd3rVcXhvQ1MtXTQiitvGEV6xvkfrXAn1dYADV1qLc2CEC/pub?output=csv'
 
 // fetch the spreadsheet as XML
 const response = await fetch(url)
-const xml = await response.text()
-
-// create an XML parser
-const parser = new Parser({ /* options */ })
-const root = parser.parse(xml)
-
-// Go through data entries
-const entries = root.getChild('feed')?.find(['entry'])
-if (entries) {
-    for (const entry of entries) {
-        const cell = entry.getChild('cell')
-        console.log(cell?.getAttr('row'), ",", cell?.getAttr('col'))
-        console.log(cell?.getValue(''))
-    }
-}
+const csv = await response.text()
+console.log(csv);
